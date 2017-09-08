40937cbf6cf20b57e8d93ccbdd608e68524957d8
Kaitlyn Folsom <kaitlynfolsom@Kaitlyns-MBP.fios-router.home>
2017-09-07 21:35:12 -0400

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,13 @@
 
 var userChoice;
 
-var correctAnswer = 0;
+var correctAnswers = 0;
 
 var incorrectAnswer = 0;
 
 var questionCount = 0;
 
-var time = 10;
+var time = 30;
 
 var intervalId;
 
@@ -18,132 +18,131 @@ var myQuestions = [
   {
     question: "What is the dominant grape in Chianti wines?",
 
-    answers: ["Sangiovese", "Dolcetto", "Zinfandel"],
+    answer1: "Sangiovese", 
+    answer2: "Dolcetto", 
+    answer3: "Zinfandel",
    
-    correctAnswer: "Sangiovese"
- 
-  },
-   {
-    question: "What is the dominant grape in Chianti wines?",
+    correctAnswer: "Sangiovese",
+  },{
+    question: "What country is generally acknowledged to be the birthplace of wine?",
 
-    answers: ["Sangiovese", "Dolcetto", "Zinfandel"],
+    answer1: "France", 
+    answer2: "Georgia", 
+    answer3: "Romania",
    
-    correctAnswer: "Sangiovese"
- 
-  },
-   {
-    question: "What is the dominant grape in Chianti wines?",
+    correctAnswer: "Georgia",
+  },{
+    question: "Rioja is a wine from Spain, but what is Rioja?",
 
-    answers: ["Sangiovese", "Dolcetto", "Zinfandel"],
+    answer1: "A variety of grape", 
+    answer2: "A region in northern Spain", 
+    answer3: "A Spanish word meaning 'King of wines'",
    
-    correctAnswer: "Sangiovese"
- 
+    correctAnswer: "A region on northern Spain",
+  },{
+    question: "In Portugal, what is a ‘Quinta’?",
+
+    answer1: "The vintage or year of harvest", 
+    answer2: "A wine-making estate", 
+    answer3: "A variety of grape used in vintage port",
+   
+    correctAnswer: "A wine-making estate",
+  },{
+    question: "Where is the wine region of Mendoza?",
+
+    answer1: "Argentina", 
+    answer2: "Chile", 
+    answer3: "Spain",
+   
+    correctAnswer: "Argentina",
+  },{
+    question: "Trichloranisole is more commonly known as what?",
+
+    answer1: "Residual Sugar", 
+    answer2: "Fruit Acid", 
+    answer3: "Cork Taint",
+   
+    correctAnswer: "Cork Taint",
   }];
-
 
 //Timer functions
 function run() {
   if(!isRunning){
     intervalId = setInterval(decrement, 1000);
     isRunning = true;
-    $("#start").hide();
-
+    $("#start-page").hide();
   }
-  if (isRunning = true){
-  $("#quiz").removeClass("display");
-  }
+    if (isRunning = true){
+    $("#quiz").removeClass("quizDisplay");
+    }
+  
+  loadQuestion();
 }
 
 function decrement() {
   time--;
   $("#timer").html("Remaining Time: " + time);
     if (time === 0) {
-      stop();
-      console.log("Time Up!");
+    stop();
+    alert("You ran out of time!");
     }
 }
 
 function stop() {
   clearInterval(intervalId);
   isRunning = false;
-}
-
-//Show results function
-function results(){
-  $("#submit").click();
+  
+  results();
 }
 
 //Let's begin!
-function quizBegin(){
-  $("#start").click(run);
-  console.log(myQuestions[questionCount].answers);
-  shuffleAnswers(myQuestions[questionCount].answers);
-  console.log(myQuestions[questionCount].answers);
-  $('#quiz').append(myQuestions[questionCount].question);
-  for (var i = 0; i < myQuestions[questionCount].answers.length; i++) {
-    var b = $('<button class="btn pill">');
-    b.text(myQuestions[questionCount].answers[i]);
-    b.appendTo('#button'+i);
-  }
-  checkAnswer();
+$("#start").click(run);
+
+function loadQuestion(){
+  var question = myQuestions[questionCount++]
+  console.log("Answer: " + question.correctAnswer);
+  
+  $("#question").text(question.question);
+
+  $("#answer1").text(question.answer1);
+  $("#answer2").text(question.answer2);
+  $("#answer3").text(question.answer3);
 }
 
-function nextQuestion() {
-  time = 10;
-  $('#timer').html(run);
-  quizBegin();
+$("#next").click(function(){
+  getAnswer();
+    if (questionCount === myQuestions.length){
+      stop();
+     } else{
+        $('input[name="answer"]').prop('checked', false);
+        timerReset();
+        loadQuestion();
+     }
+})
+
+function timerReset(){
+  time = 31;
 }
 
-function checkAnswer() {
-  $("#button").on("click", function() {
-
-    if ($(this).text() == myQuestions[questionCount].correctAnswer) {
-      $('#result').html('That was the correct answer');
-      correctAnswer++;
-      reset();
-    } 
-    else {
-      $('#result').html('That answer was incorrect the correct answer is ' + myQuestions[questionCount].correctAnswer);
-      incorrectAnswer++;
-      reset();
-    }
-  });
-  console.log("Question Count: " + questionCount);
-  checkFinalAnswer();
+//Display Results
+function results(){
+  $("#quiz").addClass("quizDisplay");
+  $("#results").removeClass("resultsDisplay");
+  $("#correct").text("Correct Answers: " + correctAnswers);
+  $("#incorrect").text("Incorrect Answers: " + incorrectAnswer);
 }
 
-function shuffleAnswers(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-
-function empty() {
-  for (var i = 0; i < 4; i++) {
-    $('#button'+i).empty();
-  }
-  $('#question').empty();
-  $('#result').empty();
-}
-
-function checkFinalAnswer() {
-  if (questionCount === myQuestions.length -1){
-    displayResults();
-  }
-}
-
-  function reset() {
-  questionCount++;
-  clearInterval(timer);
-  setTimeout(empty, 3000);
-  setTimeout(nextQuestion, 3000);
+function getAnswer(){
+ if (userChoice = question.correctAnswer){
+    correctAnswers++;
+    console.log("Correct Answers: " + correctAnswers);
+    console.log("Incorrect Answers: " + incorrectAnswer);
+ } else {
+    incorrectAnswer++;
+    console.log("Correct Answers: " + correctAnswers);
+    console.log("Incorrect Answers: " + incorrectAnswer);
+ }
 }
 
 
 
-//https://github.com/nateodes/triviaGame/blob/master/assets/app.js
-
-quizBegin();
